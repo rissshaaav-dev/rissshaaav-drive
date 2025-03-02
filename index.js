4a2453c0dea47025eec76295ef918b101e60d54e
rissshaaav-dev <191943910+rissshaaav-dev@users.noreply.github.com>
2025-03-02 23:07:52 +0530

    set up Express server with basic routing and JSON support

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use('/auth', require('./routes/auth.route'));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
