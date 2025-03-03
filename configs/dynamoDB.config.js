2c053681c52d162aa29aadcadcafd9e4997324fe
rissshaaav-dev <191943910+rissshaaav-dev@users.noreply.github.com>
2025-03-03 17:18:40 +0530

    add DynamoDB configuration file with client setup

---
+++ configs/dynamoDB.config.js
@@ -0,0 +1,10 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
+
+const dynamoDBClient = new DynamoDBClient({
+  region: process.env.AWS_REGION,
+});
+
+const dynamoDB = DynamoDBDocumentClient.from(dynamoDBClient);
+
+module.exports = { dynamoDB, PutCommand };
