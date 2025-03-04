97a88905d2dbe017eb022fc1ccef4c17fd925669
rissshaaav-dev <191943910+rissshaaav-dev@users.noreply.github.com>
2025-03-04 11:31:07 +0530

    add delete and rename file routes to file management

--- routes/file.route.js
+++ routes/file.route.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const {listUserFiles, getFileDownloadUrl} = require("../controllers/file.controller");
+const {listUserFiles, getFileDownloadUrl, deleteFile, renameFile} = require("../controllers/file.controller");
 const authenticateUser = require("../middlewares/auth.middleware");
 
 const fileRouter = express.Router();
 
 fileRouter.get("/files", authenticateUser, listUserFiles);
 fileRouter.get("/files/download/:fileId", authenticateUser, getFileDownloadUrl);
+fileRouter.delete('/files/delete/:fileId', authenticateUser, deleteFile);
+fileRouter.put("/files/rename", authenticateUser, renameFile);
 
 module.exports = fileRouter;
