4693044d9dd89d20c3e5ed86d788447523d58729
rissshaaav-dev <191943910+rissshaaav-dev@users.noreply.github.com>
2025-03-05 13:31:48 +0530

    add Swagger documentation for file upload route

--- routes/upload.route.js
+++ routes/upload.route.js
@@ -5,6 +5,54 @@ const uploadFiles = require("../controllers/upload.controller");
 
 const uploadRouter = express.Router();
 
+/**
+ * @swagger
+ * /upload:
+ *   post:
+ *     summary: Upload files to S3
+ *     description: Allows authenticated users to upload multiple files (max 10) to S3. Saves metadata in DynamoDB.
+ *     tags:
+ *       - Files
+ *     security:
+ *       - BearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               files:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *                   format: binary
+ *     responses:
+ *       200:
+ *         description: Files uploaded successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 uploadedFiles:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       fileId:
+ *                         type: string
+ *                       fileName:
+ *                         type: string
+ *                       s3Path:
+ *                         type: string
+ *       400:
+ *         description: No files uploaded
+ *       500:
+ *         description: File upload failed
+ */
 uploadRouter.post(
   "/upload",
   authenticateUser,
