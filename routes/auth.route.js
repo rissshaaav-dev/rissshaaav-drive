b7d49b603c6d76286837000610b5862f87372f3a
rissshaaav-dev <191943910+rissshaaav-dev@users.noreply.github.com>
2025-03-05 13:31:31 +0530

    add Swagger documentation for authentication routes

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,7 +3,57 @@ const { login, callback } = require("../controllers/auth.controller");
 
 const authRouter = express.Router();
 
+/**
+ * @swagger
+ * /auth/login:
+ *   get:
+ *     summary: Redirects to Cognito login page
+ *     description: Initiates the OAuth login process by redirecting the user to the Cognito-hosted UI.
+ *     tags:
+ *       - Authentication
+ *     responses:
+ *       302:
+ *         description: Redirects to Cognito login page
+ */
+
 authRouter.get("/login", login);
+
+/**
+ * @swagger
+ * /auth/callback:
+ *   get:
+ *     summary: Handles Cognito authentication callback
+ *     description: Exchanges the authorization code for access and ID tokens, then fetches user details.
+ *     tags:
+ *       - Authentication
+ *     parameters:
+ *       - in: query
+ *         name: code
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Authorization code received from Cognito after login.
+ *     responses:
+ *       200:
+ *         description: Successful authentication
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 user:
+ *                   type: object
+ *                 id_token:
+ *                   type: string
+ *                 access_token:
+ *                   type: string
+ *       400:
+ *         description: Missing authorization code
+ *       500:
+ *         description: Authentication failed
+ */
 authRouter.get("/callback", callback);
 
 module.exports = authRouter;
